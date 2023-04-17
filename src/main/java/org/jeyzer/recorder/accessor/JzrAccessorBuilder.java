72ef246e9c612095e1534798e013c9329f468bbe
Jeyzer Admin <admin@jeyzer.org>
2023-04-17 06:55:24 +0200

    Move to version 3

--- src/main/java/org/jeyzer/recorder/accessor/JzrAccessorBuilder.java
+++ src/main/java/org/jeyzer/recorder/accessor/JzrAccessorBuilder.java
@@ -4,7 +4,7 @@ package org.jeyzer.recorder.accessor;
  * ---------------------------LICENSE_START---------------------------
  * Jeyzer Recorder
  * --
- * Copyright (C) 2020, 2023 Jeyzer
+ * Copyright (C) 2020 - 2023 Jeyzer
  * --
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
